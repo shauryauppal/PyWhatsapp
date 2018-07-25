ba46d35c7be2e2addf714ea8a4242c76f4d8d2f8
shaurya uppal <shauryauppal00111@gmail.com>
2018-07-26 01:06:13 +0530

    Readme.Md added PyWhatapp modified and driver added

--- PyWhatsapp.py
+++ PyWhatsapp.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
 import time
 
 browser = None
@@ -14,11 +16,16 @@ wait = None
 
 def input_contacts():
     global Contact
-    Contact = ['"Shaurya"', '"Swadha"']
+    Contact = ['"Shaurya"', '"Shaurya new"']
     n = int(input('Enter number of Contacts to add->'))
     for i in range(0,n):
         inp = str(input("Enter contact name->"))
+        inp = '"' + inp + '"'
+        # print (inp)
         Contact.append(inp)
+    # Print the new Contact list after additional input contacts
+    print(Contact)
+    print('')
 
 
 def input_message():
@@ -28,7 +35,7 @@ def input_message():
 
 def whatsapp_login():
     global wait,browser,Link
-    browser = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\chromedriver.exe")
+    browser = webdriver.Chrome()
     wait = WebDriverWait(browser, 600)
     browser.get(Link)
     browser.maximize_window()
@@ -36,21 +43,26 @@ def whatsapp_login():
 
 def send_message(target):
     global message,wait, browser
-    x_arg = '//span[contains(@title,' + target + ')]'
-    group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
-    group_title.click()
-    input_box = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-    input_box.send_keys(message + Keys.ENTER)
-    time.sleep(1)
+    try:
+        x_arg = '//span[contains(@title,' + target + ')]'
+        group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+        group_title.click()
+        input_box = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+        input_box.send_keys(message + Keys.ENTER)
+        time.sleep(1)
+    except NoSuchElementException:
+        return
+
+
+
 
 def sender():
     global Contact
     for i in Contact:
         send_message(i)
 
-schedule.every().Monday.at("06:00").do(sender)
-schedule.every().Tuesday.at("07:00").do(sender)
-schedule.every().Friday.at("07:30").do(sender)
+schedule.every().monday.at("06:00").do(sender)
+schedule.every().wednesday.at("13:15").do(sender)
 schedule.every().day.at("23:30").do(sender)
 
 # To schedule your msgs
--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # PyWhatsapp
-Python Automation using selenium &amp; Scheduling of messages
+##  Python Automation using selenium &amp; Scheduling of messages
+
+## Objective:
+ Pywhatsapp is used to Automate Whatsapp through Whatsapp web. We can add number of contacts whom we want to send messages. Selenium and Schedule have been used one from Automation and other for Scheduling messages.
+
+------------------
+
+### Use Case:
+ We can schedule Good Morning or Good night messages at a particular time for are loved ones. We can set reminders. Suppose at 12 o'clock you want to wish your friend happy birthday so schedule your messages sleep nicely.
+
+---------------------
+## Install
+
+>$ pip install -r requirements.txt
+
+OR
+
+>$ pip install selenium
+
+>$ pip install schedule
+
+------------------
+
+## Code:
+#### input_contacts()
+
+In this functions Contacts list can be hardcoded or you can give input accordingly.(Make changes in Contact array according to you)
+
+>Enter number of Contacts to add-> 2
+
+>Enter contact names-> "Rahul"
+
+>Enter contact name-> "Shauryauppal"
+
+#### input_message()
+In this function we take input of message to send to all the Contacts list from user.
+
+Example:
+> Enter the msg to send-> Good morning
+
+#### Schedule messages
+schedule.every().Monday.at("06:00").do(sender)
+
+schedule.every().Tuesday.at("07:00").do(sender)
+
+schedule.every().Friday.at("07:30").do(sender)
+
+schedule.every().day.at("08:30").do(sender)
+
+* You make change these schedule days and time according to you.
+
+
+------------
+
+## Contributions
+<a href="https://github.com/shauryauppal/PyWhatsapp/issues"> Issues </a> and <a href ="https://github.com/shauryauppal/PyWhatsapp/pulls"> Pull requests </a> are most welcome.
+
+-------------------
+## License
+License
+Code and documentation are available according to the Apache License (see <a href="https://github.com/shauryauppal/PyWhatsapp/blob/master/LICENSE">LICENSE</a>).
+
+---------------------
+
+### Author:
+#### Shaurya Uppal
